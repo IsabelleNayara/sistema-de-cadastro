26bfe75947a350bb7b8ec6071bf859e22c544c5a
Isabelle Duarte <aecisabelle@gmail.com>
2022-10-03 22:39:39 -0300

    corrigindo o app.js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistesma de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 //mais console log de teste:
 console.log("enviando para o github")
+console.log("criando contato.html")
